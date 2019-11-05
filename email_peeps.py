c44c3c6040c62aac790e2fd588050858767f888a
Ryan Miyahara <rmiyahara144@gmail.com>
2019-11-05 13:43:12 -0800

    Need to implement email function

---
+++ email_peeps.py
@@ -0,0 +1,3 @@
+def send(send_to, their_person):
+    print(send_to + " is getting something for " + their_person) # For testing
+    return 0
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import read_sheet
-
 import random
+
+import read_sheet
+import email_peeps
 
 def main():
     emails = read_sheet.email_dict() # Holds { name: emails }
@@ -8,10 +9,16 @@ def main():
     names = read_sheet.sheet_to_list() # Holds all names in a list
     random.shuffle(names) # Names are now in a shuffled order
 
-    return 0
+    for i in range(len(names)):
+        if i == (len(names) - 1): # Case for last name
+            email_peeps.send(names[i], names[0])
+            return 0 # End here
+        else: # The rest of them
+            email_peeps.send(names[i], names[i + 1])
     
+    return 1 # You shouldn't get here
 
-
+    
 if __name__ == "__main__":
     # execute only if run as a script
     main()
