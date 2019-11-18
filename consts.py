9e35c49a92f061928f0071dd6358f640d31f85a3
Ryan Miyahara <rmiyahara144@gmail.com>
2019-11-18 00:09:38 -0800

    Delete consts.py

--- consts.py
+++
@@ -1,8 +0,0 @@
-file_name = "roster.xlsx" # Set name of roster file here
-
-col_names = 0 # Column value which holds names in the file (A = 0, B = 1, C = 2, ...)
-col_emails = 3 # Column value which holds emails in the file (A = 0, B = 1, C = 2, ...)
-
-subject = "[Kyodo] Your Secret Santa Assignment"
-host_email = "hohohoitsryan@gmail.com"
-host_password = "hoeshoeshoes"
